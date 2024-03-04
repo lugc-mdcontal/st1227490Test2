b2f283a1a76c159fbf7a9cefa0d4371e1fba95e5
Matthew Daniel Contaldi <mdcontal@lakeheadu.ca>
2024-03-04 14:23:05 -0500

    message 3(a,b)

--- src/main/java/com/java/st1227490test2/HelloApplication.java
+++ src/main/java/com/java/st1227490test2/HelloApplication.java
@@ -3,6 +3,7 @@ package com.java.st1227490test2;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,27 +12,14 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 657, 350);
+
+        stage.setTitle("Library!");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        Library library = new Library("60 Worsley St, Barrie, ON, L4M 1L6");
-        Book book1 = new Book("Intro to Java","Dan Wright","1234567891011",79.99,"TextBook");
-        Book book2 = new Book("The Maid","Nita Prose","9780735245259",24.95,"Fiction");
-        Book book3 = new Book("Atomic Habits","James Clear","9780735211292",21.88,"Self Help");
-        Book book4 = new Book("Plant You","Carleigh Bodrug","9780306923043",38.00,"CookBook");
-        Book book5 = new Book("Five Little Indians","Michelle Good","9781443459181",18.39,"Indigenous");
-        Book book6 = new Book("The Christie Affair","Nina De Gramont","9781250282132",38.00,"Romance");
-        library.addBook(book1);
-        library.addBook(book2);
-        library.addBook(book3);
-        library.addBook(book4);
-        library.addBook(book5);
-        library.addBook(book6);
-
         launch();
     }
 }
--- src/main/java/com/java/st1227490test2/HelloController.java
+++ src/main/java/com/java/st1227490test2/HelloController.java
@@ -1,14 +1,89 @@
 package com.java.st1227490test2;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+import java.util.ArrayList;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    private Library library;
+    private int currentBookIndex = 0;
+
+    public void renderBook() {
+        // Get the books
+        ArrayList<Book> books = library.getBooks();
+
+        // Make sure it's valid range
+        if (currentBookIndex < 0 || currentBookIndex > books.size())
+            throw new IllegalArgumentException("The book is in invalid.");
+
+        Book fetchedBook = books.get(currentBookIndex);
+
+        title.setText(fetchedBook.getTitle());
+        author.setText(fetchedBook.getAuthor());
+        isbn.setText(fetchedBook.getIsbn());
+        category.setText(fetchedBook.getCategory());
+        price.setText(String.format("%.2f", fetchedBook.getPrice()));
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label address;
+
+    @FXML
+    private Label author;
+
+    @FXML
+    private Label category;
+
+    @FXML
+    private Label isbn;
+
+    @FXML
+    private Button nextBook;
+
+    @FXML
+    private Label numBooks;
+
+    @FXML
+    private Label price;
+
+    @FXML
+    private Label title;
+
+    @FXML
+    private Label valBooks;
+
+    @FXML
+    void onNextButton(MouseEvent event) {
+        // This makes sure we never go over, and just reloops
+        currentBookIndex = (++currentBookIndex) % library.getBooks().size();
+
+        // Render the book
+        renderBook();
+    }
+
+    @FXML
+    public void initialize() {
+        library = new Library("60 Worsley St, Barrie, ON, L4M 1L6");
+        Book book1 = new Book("Intro to Java","Dan Wright","1234567891011",79.99,"TextBook");
+        Book book2 = new Book("The Maid","Nita Prose","9780735245259",24.95,"Fiction");
+        Book book3 = new Book("Atomic Habits","James Clear","9780735211292",21.88,"Self Help");
+        Book book4 = new Book("Plant You","Carleigh Bodrug","9780306923043",38.00,"CookBook");
+        Book book5 = new Book("Five Little Indians","Michelle Good","9781443459181",18.39,"Indigenous");
+        Book book6 = new Book("The Christie Affair","Nina De Gramont","9781250282132",38.00,"Romance");
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+        library.addBook(book4);
+        library.addBook(book5);
+        library.addBook(book6);
+
+        address.setText(address.getText() + library.getAddress());
+        numBooks.setText(numBooks.getText() + library.getBooks().size());
+        valBooks.setText(valBooks.getText() + String.format("%.2f", library.inventoryValue()));
+
+        renderBook();
     }
 }
