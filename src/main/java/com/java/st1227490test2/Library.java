dd2a0ee5ceda4d9b7f7a01639316627cb85e5717
Matthew Daniel Contaldi <mdcontal@lakeheadu.ca>
2024-03-04 13:35:40 -0500

    message 2(a) complete

---
+++ src/main/java/com/java/st1227490test2/Library.java
@@ -0,0 +1,12 @@
+package com.java.st1227490test2;
+
+import java.util.ArrayList;
+
+public class Library {
+    private String address;
+    private ArrayList<Book> books = new ArrayList<Book>();
+
+    public Library(String address, ArrayList<Book> books) {
+        
+    }
+}
