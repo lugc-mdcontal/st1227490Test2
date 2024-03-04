1a6c7a33352f45db09625f3c6686b9a44a2b90ec
Matthew Daniel Contaldi <mdcontal@lakeheadu.ca>
2024-03-04 13:13:05 -0500

    message 1(a) complete

---
+++ src/main/java/com/java/st1227490test2/Book.java
@@ -0,0 +1,9 @@
+package com.java.st1227490test2;
+
+public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private String category;
+    private double price;
+}
