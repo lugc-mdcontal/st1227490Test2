f1ba2150193da4e45e30e9fca9034ae6827bb7a6
Matthew Daniel Contaldi <mdcontal@lakeheadu.ca>
2024-03-04 13:15:54 -0500

    message 1(b)

--- src/main/java/com/java/st1227490test2/Book.java
+++ src/main/java/com/java/st1227490test2/Book.java
@@ -6,4 +6,17 @@ public class Book {
     private String isbn;
     private String category;
     private double price;
+
+    public String getTitle() { return title; }
+    public void setTitle(String newTitle) {
+        // Ensure the string is correct length
+        if (newTitle.length() < 4)
+            throw new IllegalArgumentException("The title needs to be at least four characters in length.");
+
+        // Clean up the title string
+        newTitle = newTitle.trim();
+
+        // Set the title in this class
+        title = newTitle;
+    }
 }
